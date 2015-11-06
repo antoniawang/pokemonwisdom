46374dfeaa931bc838d7e342c4193a1998373738
Antonia Wang <antonia.t.wang@gmail.com>
2015-11-06 00:09:33 -0800

    elimated timer loop; will move to server

--- tweeting.py
+++ tweeting.py
@@ -2,19 +2,17 @@ import os, time, sys
 import twitter
 from pokemon_wisdom import create_wisdom
 
-while True:
-	api = twitter.Api(
-	    consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
-	    consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
-	    access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
-	    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+api = twitter.Api(
+    consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+    consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+    access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
 
-	#check if credentials are correct
-	print api.VerifyCredentials()
+#check if credentials are correct
+print api.VerifyCredentials()
 
-	#send a tweet every 24 hours
-	status = create_wisdom()
-	print status
+#send a tweet every 24 hours
+status = create_wisdom()
+print status
 
-	api.PostUpdate(status)
-	time.sleep(86400)
+api.PostUpdate(status)
